a174fb79dc42786a2db91de0d80d4627a1535655
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-22 22:26:12 +0100

    added beforeEach()

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,30 @@
 import { TurboNumber } from "./index";
 
 describe(TurboNumber.name, () => {
+  let tn: TurboNumber;
+  beforeEach(() => {
+    tn = new TurboNumber(10);
+  });
+
   it("returns the provided number if no operations are made", () => {
-    expect(new TurboNumber(10).result()).toBe(10);
+    expect(tn.result()).toBe(10);
   });
 
   it("subtracts a number from the saved number", () => {
-    const tn = new TurboNumber(10);
     tn.subtract(6);
     expect(tn.result()).toBe(4);
   });
 
   it("divides the saved number by the number provided", () => {
-    const tn = new TurboNumber(10);
     tn.divide(5);
     expect(tn.result()).toBe(2);
   });
 
   it("allows chaining", () => {
-    const tn = new TurboNumber(10);
     expect(tn.divide(5).subtract(1).result()).toBe(1);
   });
 
   it("doesn't allow division by zero", () => {
-    const tn = new TurboNumber(10);
     expect(() => tn.divide(0)).toThrow("You cannot divide by zero!");
   });
 
